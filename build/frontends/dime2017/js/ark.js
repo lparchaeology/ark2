8cddda06828def5f307e7b3e11245bc3b13afed5
John Layt <john@layt.net>
2017-01-18 16:59:28 +0000

    WIP Fix select box widths

--- build/frontends/dime2017/js/ark.js
+++ build/frontends/dime2017/js/ark.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
+    // Replace all select tags with Select2
     $("select").select2();
+    // HACK To fix Select2 not being responsive
+    // See https://github.com/select2/select2/issues/3278 and http://stackoverflow.com/a/41429176
+    $(".select2.select2-container").css("width", "100%");
+    // FIXME Hide Bootstrap Table loading animation as is a bit broken
     $('.table-bootstrap-table').bootstrapTable('hideLoading');
 });
+
 jQuery(function($) {
     $('form[data-async]').on('submit', function(event) {
         var $form = $(this);
