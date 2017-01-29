6298d4276abb7a8648fd2be1462e28dc8438cb10
John Layt <john@layt.net>
2017-01-29 18:45:27 +0000

    WIP Fix redirect

--- src/DIME/php/Action/EntityAction.php
+++ src/DIME/php/Action/EntityAction.php
@@ -46,6 +46,6 @@ class EntityAction extends DimeFormAction
         ORM::persist($item);
         ORM::flush('data');
         $path = Service::path($redirect, ['itemSlug' => $item->id()]);
-        return Service::renderResponse($path);
+        return Service::redirectPath($path);
     }
 }
