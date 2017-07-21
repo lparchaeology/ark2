eb9425c4aab7c93d23777a29b79f7967f89af764
John Layt <john@layt.net>
2017-07-21 16:14:02 +0100

    Add users to info script

--- src/ARK/server/php/Site/Console/SiteMigrateInfoCommand.php
+++ src/ARK/server/php/Site/Console/SiteMigrateInfoCommand.php
@@ -44,6 +44,7 @@ class SiteMigrateInfoCommand extends DatabaseCommand
     protected $modules = [];
     protected $itemkeys = [];
     protected $sites = [];
+    protected $users = [];
     protected $types = [];
     protected $tables = [];
     protected $frags = [];
@@ -64,21 +65,15 @@ class SiteMigrateInfoCommand extends DatabaseCommand
         $this->source->beginTransaction();
 
         // SITES
-        $this->write("\nSITES");
-        $this->sites = $this->source->fetchAllTable('cor_tbl_ste');
-        $table = new Table($this->output);
-        $table->setHeaders(['ID', 'Description']);
-        foreach ($this->sites as $site) {
-            $table->addRow([$site['id'], $site['description']]);
+        $sites = $this->source->fetchAllTable('cor_tbl_ste');
+        foreach ($sites as $site) {
+            $site['modules'] = [];
+            $site['items'] = 0;
+            $this->sites[$site['id']] = $site;
         }
-        $table->render();
-        $this->write('');
 
         // MODULES
         $this->modules = $this->source->fetchAllTable('cor_tbl_module');
-        $this->write("\nMODULES");
-        $table = new Table($this->output);
-        $table->setHeaders(['Code', 'Description', 'Modtype', 'Table', 'Rows']);
         foreach ($this->modules as &$module) {
             $mod = $module['shortform'];
             if ($mod == 'cor') {
@@ -102,10 +97,22 @@ class SiteMigrateInfoCommand extends DatabaseCommand
                 $module['lut'] = null;
                 $module['modtypes'] = [];
             }
-            $table->addRow([$module['shortform'], $module['description'], $module['modtype'], $module['table'], $module['rows']]);
+            foreach (array_keys($this->sites) as $id) {
+                $count = $this->countItems($module['table'], $id);
+                $this->sites[$id]['modules'][$mod] = $count;
+                $this->sites[$id]['items'] += $count;
+            }
         }
-        $table->render();
-        $this->write('');
+        unset($module);
+
+        // USERS
+        $users = $this->source->fetchAllTable('cor_tbl_users');
+        foreach ($users as $user) {
+            $user['audit'] = false;
+            $user['action'] = false;
+            $this->users[$user['id']] = $user;
+        }
+        unset($user);
 
         // ACTIONS
         $this->buildTypes('action');
@@ -114,17 +121,22 @@ class SiteMigrateInfoCommand extends DatabaseCommand
             FROM cor_tbl_action, cor_lut_actiontype
             WHERE cor_tbl_action.actiontype = cor_lut_actiontype.id
         ';
-        $this->countFrags('action', $sql);
+        $this->summariseFrags('action', $sql);
 
         // ATTRIBUTES
-        $this->buildTypes('attribute');
+        $sql = '
+            SELECT cor_lut_attribute.*, cor_lut_attributetype.attributetype, cor_lut_attributetype.module
+            FROM cor_lut_attribute, cor_lut_attributetype
+            WHERE cor_lut_attribute.attributetype = cor_lut_attributetype.id
+        ';
+        $this->buildTypes('attribute', $sql);
         $sql = '
             SELECT cor_tbl_attribute.*, cor_lut_attribute.attribute, cor_lut_attributetype.attributetype
             FROM cor_tbl_attribute, cor_lut_attribute, cor_lut_attributetype
             WHERE cor_tbl_attribute.attribute = cor_lut_attribute.id
             AND cor_lut_attribute.attributetype = cor_lut_attributetype.id
         ';
-        $this->countFrags('attribute', $sql);
+        $this->summariseFrags('attribute', $sql);
 
         // FILES
         $this->buildTypes('file');
@@ -134,25 +146,25 @@ class SiteMigrateInfoCommand extends DatabaseCommand
             WHERE cor_tbl_file.file = cor_lut_file.id
             AND cor_lut_file.filetype = cor_lut_filetype.id
         ';
-        $this->countFrags('file', $sql);
+        $this->summariseFrags('file', $sql);
 
         // DATE
         $this->buildTypes('date');
-        $this->countTypeFrags('date');
+        $this->summariseTypeFrags('date');
 
         // NUMBER
         $this->buildTypes('number');
-        $this->countTypeFrags('number');
+        $this->summariseTypeFrags('number');
 
         // TEXT
         $this->buildTypes('txt');
-        $this->countTypeFrags('txt');
+        $this->summariseTypeFrags('txt');
 
         // SPAN
         $this->buildTypes('span');
-        $this->countTypeFrags('span');
+        $this->summariseTypeFrags('span');
 
-        // * COPY XMIS * //
+        // XMI
         $frags = $this->source->fetchAllTable('cor_tbl_xmi');
         foreach ($frags as $frag) {
             $mod = $frag['itemkey'];
@@ -169,41 +181,94 @@ class SiteMigrateInfoCommand extends DatabaseCommand
             }
         }
 
-        // MODULE SCHEMA
+        // REPORT SITES
+        $this->write("\nSITES");
+        $table = new Table($this->output);
+        $table->setHeaders(['ID', 'Description', 'Items']);
+        foreach ($this->sites as $site) {
+            $table->addRow([$site['id'], $site['description'], $site['items']]);
+        }
+        $table->render();
+        $this->write('');
+
+        // REPORT MODULES
+        $this->write("\nMODULES");
+        $table = new Table($this->output);
+        $table->setHeaders(['Code', 'Description', 'Modtype', 'Table', 'Rows']);
+        foreach ($this->modules as $module) {
+            if (isset($module['shortform']) && $module['shortform'] != 'cor') {
+                $table->addRow([$module['shortform'], $module['description'], $module['modtype'], $module['table'], $module['rows']]);
+            }
+        }
+        $table->render();
+        $this->write('');
+
+        // REPORT SITE MODULES
+        $this->write("\nMODULES AS USED BY SITES");
+        $table = new Table($this->output);
+        $table->setHeaders(['ID', 'Module', 'Items']);
+        foreach ($this->sites as $site) {
+            foreach ($site['modules'] as $module => $items) {
+                if ($items > 0) {
+                    $table->addRow([$site['id'], $module, $items]);
+                }
+            }
+        }
+        $table->render();
+        $this->write('');
+
+        // REPORT USERS
+        $this->write("\nUSERS");
+        $table = new Table($this->output);
+        $table->setHeaders(['Username', 'Name', 'Audit', 'Action']);
+        foreach ($this->users as $id => $user) {
+            if (isset($user['username'])) {
+                $table->addRow([$user['username'], $user['firstname'].' '.$user['lastname'], $user['audit'], $user['action']]);
+            } else {
+                $table->addRow([$id, 'Unknown', $user['audit'] ?? false, $user['action'] ?? false]);
+            }
+        }
+        $table->render();
+        $this->write('');
+
+        // REPORT FRAG SCHEMA
+        $this->write("\nTYPES AS USED BY MODULES");
         foreach ($this->modules as &$module) {
-            if ($module['shortform'] == 'cor' || !isset($this->frags[$module['itemkey']])) {
+            if (!isset($module['shortform']) || $module['shortform'] == 'cor' || !isset($this->frags[$module['itemkey']])) {
                 continue;
             }
             $module['frags'] = $this->frags[$module['itemkey']];
+            ksort($module['frags']);
             $this->write("\n".$module['shortform']);
             $table = new Table($this->output);
             $table->setHeaders(['Class', 'Field', 'Count']);
-            foreach ($module['frags'] as $class => $fields) {
+            foreach ($module['frags'] as $class => &$fields) {
+                ksort($fields);
                 foreach ($fields as $field => $count) {
                     $table->addRow([$class, $field, $count]);
                 }
             }
             $table->render();
-            $this->write('');
         }
 
-        // TYPES
-        $this->write("\nThe following attribute types exist:");
-        $this->types = $this->source->fetchAllTable('cor_lut_attributetype');
-        foreach ($this->types as $type) {
-            $sql = '
-                SELECT cor_lut_attribute.*, cor_lut_attributetype.attributetype
-                FROM cor_lut_attribute, cor_lut_attributetype
-                WHERE cor_lut_attribute.attributetype = ?
-                AND cor_lut_attribute.attributetype = cor_lut_attributetype.id
-            ';
-            $this->types['values'] = $this->source->fetchAll($sql, [$type['id']]);
-            $this->write("\n  ".$type['attributetype'].' ['.$type['module'].'] ('.count($this->types['values']).')');
-            foreach ($this->types['values'] as $value) {
-                $this->write('    '.$value['attribute']);
+        // REPORT FULL SCHEMA
+        $this->write("\nTYPES AS DEFINED BY MODULES");
+        ksort($this->types);
+        foreach ($this->types as $module => &$types) {
+            $this->write("\n".$module);
+            $table = new Table($this->output);
+            $table->setHeaders(['Class', 'Field', 'Value', 'Count']);
+            foreach ($types as $class => &$fields) {
+                ksort($fields);
+                foreach ($fields as $field => &$values) {
+                    ksort($values);
+                    foreach ($values as $value => $count) {
+                        $table->addRow([$class, $field, $value, $count]);
+                    }
+                }
             }
+            $table->render();
         }
-        $this->write('');
     }
 
     private function getItemkey($frag)
@@ -232,7 +297,60 @@ class SiteMigrateInfoCommand extends DatabaseCommand
         return $frag['itemkey'];
     }
 
-    private function countTypeFrags($type)
+    private function countItems($table, $site = null)
+    {
+        $sql = "
+            SELECT COUNT(*)
+            FROM $table
+        ";
+        $parms = [];
+        if ($site) {
+            $sql .= 'WHERE ste_cd = ?';
+            $parms[] = $site;
+        }
+
+        return $this->source->executeQuery($sql, $parms)->fetch()['COUNT(*)'];
+    }
+
+    private function countTypeFrags($type, $field = null)
+    {
+        if ($type == 'file') {
+            return null;
+        }
+        $typename = $type.'type';
+        $sql = "
+            SELECT COUNT(*)
+            FROM cor_tbl_$type, cor_lut_$typename
+            WHERE cor_tbl_$type.$typename = cor_lut_$typename.id
+        ";
+        $parms = [];
+        if ($field) {
+            $sql .= "AND cor_lut_$typename.$typename = ?";
+            $parms[] = $field;
+        }
+
+        return $this->source->executeQuery($sql, $parms)->fetch()['COUNT(*)'];
+    }
+
+    private function countAttributeFrags($field, $value = null)
+    {
+        $sql = '
+            SELECT COUNT(*)
+            FROM cor_tbl_attribute, cor_lut_attribute, cor_lut_attributetype
+            WHERE cor_tbl_attribute.attribute = cor_lut_attribute.id
+            AND cor_lut_attribute.attributetype = cor_lut_attributetype.id
+            AND cor_lut_attributetype.attributetype = ?
+        ';
+        $parms[] = $field;
+        if ($value) {
+            $sql .= 'AND cor_lut_attribute.attribute = ?';
+            $parms[] = $value;
+        }
+
+        return $this->source->executeQuery($sql, $parms)->fetch()['COUNT(*)'];
+    }
+
+    private function summariseTypeFrags($type, $field = null)
     {
         $typename = $type.'type';
         $sql = "
@@ -240,32 +358,53 @@ class SiteMigrateInfoCommand extends DatabaseCommand
             FROM cor_tbl_$type, cor_lut_$typename
             WHERE cor_tbl_$type.$typename = cor_lut_$typename.id
         ";
-        $this->countFrags($type, $sql);
+        $parms = [];
+        if ($field) {
+            $sql .= "AND $typename = ?";
+            $parms[] = $field;
+        }
+        $this->summariseFrags($type, $sql);
     }
 
-    private function countFrags($type, $sql)
+    private function summariseFrags($type, $sql, $parms = [])
     {
-        $frags = $this->source->fetchAll($sql);
+        $frags = $this->source->fetchAll($sql, $parms);
         $typename = $type.'type';
         foreach ($frags as $frag) {
-            $modcode = $this->getItemkey($frag);
+            $module = $this->getItemkey($frag);
             $field = $frag[$typename];
-            if (isset($this->frags[$modcode][$type][$field])) {
-                $this->frags[$modcode][$type][$field] += 1;
+            if (isset($this->frags[$module][$type][$field])) {
+                $this->frags[$module][$type][$field] += 1;
             } else {
-                $this->frags[$modcode][$type][$field] = 1;
+                $this->frags[$module][$type][$field] = 1;
+            }
+            if (isset($frag['cre_by'])) {
+                $this->users[$frag['cre_by']]['audit'] = true;
+            }
+            if (isset($frag['mod_by'])) {
+                $this->users[$frag['mod_by']]['audit'] = true;
             }
         }
     }
 
-    private function buildTypes($type)
+    private function buildTypes($type, $sql = '')
     {
         $typename = $type.'type';
-        $types = $this->source->fetchAllTable('cor_lut_'.$typename);
-        foreach ($types as $type) {
-            $modcode = $type['module'];
-            $field = $frag[$typename];
-            $this->types[$modcode][$type][$field] = null;
+        $lut = 'cor_lut_'.$typename;
+        $tbl = 'cor_tbl_'.$typename;
+        if ($sql) {
+            $classes = $this->source->fetchAll($sql);
+        } else {
+            $classes = $this->source->fetchAllTable($lut);
+        }
+        foreach ($classes as $class) {
+            $module = ($class['module'] ?? 'cor');
+            $field = $class[$typename];
+            if (isset($class[$type])) {
+                $this->types[$module][$type][$field][$class[$type]] = $this->countAttributeFrags($field, $class[$type]);
+            } else {
+                $this->types[$module][$type][$field][''] = $this->countTypeFrags($type, $field);
+            }
         }
     }
 }
