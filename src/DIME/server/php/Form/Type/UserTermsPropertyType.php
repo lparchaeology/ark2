15a3cce65026c4e3751c35bc0ef7352ca5314b52
John Layt <john@layt.net>
2017-08-22 17:00:10 +0100

    Improve user registration page, add T&Cs agreement, etc

---
+++ src/DIME/server/php/Form/Type/UserTermsPropertyType.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * ARK Form Type.
+ *
+ * Copyright (C) 2017  L - P : Heritage LLP.
+ *
+ * This file is part of ARK, the Archaeological Recording Kit.
+ *
+ * ARK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ARK.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author     John Layt <j.layt@lparchaeology.com>
+ * @copyright  2017 L - P : Heritage LLP.
+ * @license    GPL-3.0+
+ * @see        http://ark.lparchaeology.com/
+ * @since      2.0
+ */
+
+namespace DIME\Form\Type;
+
+use ARK\Form\Type\AbstractPropertyType;
+use ARK\Model\Property;
+use ARK\Vocabulary\Term;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class UserTermsPropertyType extends AbstractPropertyType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options) : void
+    {
+        $field = $options['state']['field'];
+        $datatype = $field->attribute()->datatype();
+        $builder->add('agree', CheckboxType::class, ['label' => 'core.user.terms.agree', 'required' => true]);
+        $builder->add($datatype->valueName(), HiddenType::class, []);
+        $builder->setDataMapper($this);
+    }
+
+    public function mapDataToForms($property, $forms) : void
+    {
+        if (!$property) {
+            return;
+        }
+        $forms = iterator_to_array($forms);
+        $datatype = $property->attribute()->datatype();
+        $value = $property->value();
+        if ($value instanceof Term) {
+            $forms[$datatype->valueName()]->setData($value->name());
+            $forms['agree']->setData(true);
+        } else {
+            $term = $property->attribute()->vocabulary()->defaultTerm();
+            $forms[$datatype->valueName()]->setData($term->name());
+            $forms['agree']->setData(false);
+        }
+    }
+
+    public function mapFormsToData($forms, &$property) : void
+    {
+        if (!$property instanceof Property) {
+            return;
+        }
+        $forms = iterator_to_array($forms);
+        if (!$forms['agree']->getData()) {
+            return;
+        }
+        $datatype = $property->attribute()->datatype();
+        $value = $forms[$datatype->valueName()]->getData();
+        $property->setValue($value);
+    }
+
+    protected function options()
+    {
+        return [
+            'compound' => true,
+        ];
+    }
+}
