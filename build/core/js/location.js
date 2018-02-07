f02b41eef08ff4d0794221b14e186c412ac158e4
John Layt <john@layt.net>
2018-02-07 13:06:52 +0000

    WIP Work on Location module

--- build/core/js/location.js
+++ build/core/js/location.js
@@ -1,10 +1,9 @@
 // Location Picker object using Revealing Module pattern
 
-var Location = (function (baseSource, pinIconSrc, center, extent, zoom, target) {
+var Location = (function () {
 
-    var mapSrid = 3857;
-    var mapEpsg = 'EPSG:3857';
-    var baseSource = null;
+    var mapSrid = null;
+    var mapEpsg = null;
     var baseLayer = null;
     var locationSource = null;
     var locationLayer = null;
@@ -13,13 +12,75 @@ var Location = (function (baseSource, pinIconSrc, center, extent, zoom, target) 
     var mapView = null;
     var map = null;
 
-    var init = function init(baseSource, pinIconSrc, center, extent, zoom, target) {
+    var clear = function clear() {
+        this.locationSource.clear();
+    };
 
-        this.baseSource = baseSource;
+    var coordinates = function coordinates() {
+        if (this.locationSource.getFeatures().length === 1) {
+            return this.locationSource.getFeatures()[0].getGeometry().getCoordinates();
+        }
+        return [null, null];
+    };
+
+    var setCoordinates = function setCoordinates(coords) {
+        if (isNaN(coords[0]) || isNaN(coords[0])) {
+            return;
+        }
+        var feature = new ol.Feature({ geometry: new ol.geom.Point(coords), });
+        this.locationSource.clear();
+        this.locationSource.addFeature(feature);
+        this.map.getView().setCenter(coords);
+        this.map.getView().setZoom(12);
+    };
+
+    var makeDecimal = function makeDecimal(value) {
+        return parseFloat(parseFloat(value).toFixed(6));
+    };
+
+    var location = function location(srid) {
+        var coords = coordinates();
+        if (srid === null) {
+            srid = this.mapSrid;
+        } else if (srid !== this.mapSrid) {
+            coords = ol.proj.transform(coords, this.mapEpsg, 'EPSG:' + srid);
+        }
+        return {
+            'easting': makeDecimal(coords[0]),
+            'northing': makeDecimal(coords[1]),
+            'srid': srid,
+        };
+    };
+
+    var setLocation = function setLocation(location) {
+        if (isNaN(location.easting) || isNaN(location.northing)) {
+            return;
+        }
+        var coords = [location.easting, location.northing];
+        if (location.srid !== this.mapSrid) {
+            var epsg = 'EPSG:' + location.srid;
+            coords = ol.proj.transform(coords, epsg, this.mapEpsg);
+        }
+        setCoordinates(coords);
+    };
+
+    var geolocate = function geolocate() {
+        var geolocation = new ol.Geolocation({
+            projection: this.mapView.getProjection(),
+        });
+        setCoordinates(geolocation.getPosition());
+        return geolocation.getAccuracy();
+    };
+
+    var init = function (baseSource, pinIconSrc, srid, center, extent, zoom, target, ondraw) {
+
+        this.mapSrid = srid;
+        this.mapEpsg = 'EPSG:' + srid;
+
         this.baseLayer = new ol.layer.Tile({
             visible: true,
             preload: Infinity,
-            source: this.baseSource,
+            source: baseSource,
         });
 
         this.locationSource = new ol.source.Vector({ wrapX: false, });
@@ -47,6 +108,7 @@ var Location = (function (baseSource, pinIconSrc, center, extent, zoom, target) 
                 }),
             }),
         });
+        this.locationDraw.on('drawend', ondraw);
 
         this.mapView = new ol.View({
             center: center,
@@ -55,78 +117,17 @@ var Location = (function (baseSource, pinIconSrc, center, extent, zoom, target) 
             minZoom: zoom,
         });
         this.map = new ol.Map({
-            layers: [this.baseLayer, this.locationLayer,],
+            layers: [this.baseLayer, this.locationLayer, ],
             loadTilesWhileInteracting: true,
             target: target,
             view: this.mapView,
-            controls: [new ol.control.FullScreen(), new ol.control.Zoom(),],
+            controls: [new ol.control.FullScreen(), new ol.control.Zoom(), ],
         });
         this.map.addInteraction(this.locationDraw);
-
     };
-
-    var clear = function clear() {
-        this.locationSource.clear();
-    };
-
-    var coordinates = function coordinates() {
-        if (this.locationSource.getFeatures().length === 1) {
-            return this.locationSource.getFeatures()[0].getGeometry().getCoordinates();
-        }
-        return [null, null];
-    };
-
-    var setCoordinates = function setCoordinates(coords) {
-        if (isNaN(coords[0]) || isNaN(coords[0])) {
-            return;
-        }
-        var feature = new ol.Feature({ geometry: new ol.geom.Point(coords), });
-        this.clear();
-        this.locationSource.addFeature(feature);
-        this.map.getView().setCenter(coords);
-        this.map.getView().setZoom(12);
-    };
-
-    var makeDecimal = function makeDecimal(value) {
-        return parseFloat(parseFloat(value).toFixed(6));
-    };
-
-    var location = function location(srid) {
-        var coords = this.coordinates();
-        if (srid === null) {
-            srid = this.mapSrid;
-        } else if (srid !== this.mapSrid) {
-            coords = ol.proj.transform(coords, this.mapEpsg, 'EPSG:' + srid);
-        }
-        return {
-            'easting': this.makeDecimal(coords[0]),
-            'northing': this.makeDecimal(coords[1]),
-            'srid': srid,
-        };
-    };
-
-    var setLocation = function setLocation(location) {
-        if (isNaN(location.easting) || isNaN(location.northing)) {
-            return;
-        }
-        var coords = [location.easting, location.northing];
-        if (location.srid !== this.mapSrid) {
-            coords = ol.proj.transform(coords, 'EPSG:' + location.srid, this.mapEpsg);
-        }
-        this.setCoordinates(coords);
-    };
-
-    var geolocate = function geolocate() {
-        var geolocation = new ol.Geolocation({
-            projection: this.mapView.getProjection(),
-        });
-        this.setCoordinates(geolocation.getPosition());
-        return geolocation.getAccuracy();
-    };
-
-    this.init(baseSource, pinIconSrc, center, extent, zoom, target);
 
     return {
+        init: init,
         location: location,
         setLocation: setLocation,
         clear: clear,
