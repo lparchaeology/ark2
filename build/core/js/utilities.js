d2b8946051103f34f3a29cc54e81f79d299c9039
John Layt <john@layt.net>
2018-02-27 21:47:18 +0000

    Make share buttons functional

--- build/core/js/utilities.js
+++ build/core/js/utilities.js
@@ -17,3 +17,9 @@ function debounce(func, wait, immediate) {
         }
     };
 }
+
+function windowPopup(url, width, height) {
+    var left = (screen.width / 2) - (width / 2);
+    var top = (screen.height / 2) - (height / 2);
+    window.open(url, "", "width=" + width + ",height=" + height + ",top=" + top + ",left=" + left);
+}
