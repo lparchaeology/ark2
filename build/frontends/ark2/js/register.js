95c493e9870bd48ad110e800487fd299a1a220a2
John Layt <j.layt@lparchaeology.com>
2018-04-09 15:43:10 +0100

    Remove ark2 and bootstrap3 frontends not used by DIME

--- build/frontends/ark2/js/register.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-    $(".add-module-selector").on("click", function () {
-        console.log({ "data": $(this).attr("data-module") });
-        $(".record-add-form").hide()
-        $(".record-add-form").filter("." + $(this).attr("data-module")).show();
-    });
-
-});
